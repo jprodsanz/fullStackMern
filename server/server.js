e84e08592d5166230c85afbc3532d883ebe831dd
Pablo X <jprodsanz@gmail.com>
2023-01-12 21:46:27 -0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) =>{
+    res.send('Your first API has been created');
+});
+
+// require('./config/mongoose.config')
+
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+
+// const AllMyJokeRoutes = require('./routes/jokes.routes')
+// AllMyJokeRoutes(app)
+
+const  PORT = process.env.PORT || 5000;
+app.listen(PORT,console.log
+    (`Hi Pablo Diez, your server's running on PORT ${PORT}! Would you like to proceed?`));
